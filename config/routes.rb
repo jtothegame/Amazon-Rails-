02294488e80d84346d979a443870efad1b2b421b
Josh Gamble <josh.gamble33@gmail.com>
2017-04-04 15:01:25 -0700

    Complete User Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
   root 'welcome#index'
 
 end
